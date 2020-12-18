6fcb4964d2791cdecd96ddefe4f30f208fd04af8
Nathan Brown <nathan@upventrix.com>
2020-12-18 17:41:05 -0500

    remove in progress image

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -15,9 +15,6 @@ class Resume extends Component {
         return <div key={work.company}><h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
             <p>{work.description}</p>
-            {work.image ? (
-                <img alt="loading-accomplishments" src={work.image}/>
-            ):(null)}
         </div>
       })
       var skills = this.props.data.skills.map(function(skills){
